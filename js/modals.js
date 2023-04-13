dff002ff77efd1330475a6bfa3dffc5a39cd6091
nimborlimbo <nimborlimbo@gmail.com>
2023-04-13 16:36:48 +0500

    quiz-fixed

--- js/modals.js
+++ js/modals.js
@@ -7,11 +7,12 @@ function createElementByHTML(tagName, className, innerHTML)
 }
 
 class Modal {
-    window = createElementByHTML('div', 'modal', '');
-    modal = createElementByHTML('div', 'modal__block', '');
-    close = createElementByHTML('div', 'modal-close', '<hr><hr>');
+    window; modal; close;
 
     constructor() {
+        this.window = createElementByHTML('div', 'modal', '');
+        this.modal = createElementByHTML('div', 'modal__block', '');
+        this.close = createElementByHTML('div', 'modal-close', '<hr><hr>');
         if (this.constructor == Modal)
             throw new Error('cannot use an abstract class');
     }
@@ -239,7 +240,7 @@ var calcArray =
                 title: 'Сколько зубов Вы хотите вылечить',
                 items: 
                 [
-                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10 и более', 'Нужна консультация'
+                    '1', '2', '3', '4', '5', '6 и более', 'Нужно подготовить к имплантации/протезированию', 'Нужна консультация врача'
                 ]
             },
 
@@ -278,10 +279,10 @@ var calcArray =
 
         [
             {
-                title: 'Сколько зубов Вы хотите вылечить',
+                title: 'Коронки из какого материала Вам предпочтительнее',
                 items: 
                 [
-                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10 и более', 'Нужна консультация'
+                    'Металлические', 'Металлокерамика', 'Керамика', 'Пластмасса', 'Цирконий', 'Нужна помощь врача', '7', '8', '9', '10 и более', 'Нужна консультация'
                 ]
             },
 
@@ -657,109 +658,242 @@ var calcArray =
 };
 
 class CalculatorList {
-    #title;
-    #itemsList;
-    #listBlock; #listArray = [];
-    
-    constructor(title, itemsList) {
+    #title; #listElement; 
+    #itemsArray; #itemsElementsArray = [];
+    #value; #valueId = null;
+
+    constructor(title, itemsArray) {
         this.#title = title;
-        this.#itemsList = itemsList;
-        this.#listBlock = this.#createList(itemsList);
+        this.#itemsArray = itemsArray;
+        this.#createList();
     }
 
-    getItemsArray() {
-        return this.#listArray;
-    }
+    #createList() {
+        this.#listElement = createElementByHTML('div', 'calculator__main__list', '');
 
-    getList() {
-        return this.#listBlock;
-    }
+        this.#itemsArray.forEach(item => {
+            let itemHtmlCode = `<div class="calculator__main__list__item__check"></div><p class="calculator__main__list__item__name">${item}</p>`;
 
-    #createList(itemsList) {
-        let list = createElementByHTML('div', 'calculator__main__list', '');
+            let itemElement = createElementByHTML('div', 'calculator__main__list__item', itemHtmlCode);
 
-        itemsList.forEach(item => {
-            let itemElemHtml = `<div class='calculator__main__list__item__check'></div><p class='calculator__main__list__item__name'>${item}</p>`;
-            let itemElem = createElementByHTML('div', 'calculator__main__list__item', itemElemHtml);
-            this.#listArray.push(itemElem);
-            list.insertAdjacentElement('beforeend', itemElem);
+            this.#itemsElementsArray.push(itemElement);
+
+            this.#listElement.insertAdjacentElement('beforeend', itemElement);
         });
 
-        this.#listArray.forEach(item => {
-            item.addEventListener('click', () => {
-                this.#listArray.forEach(itemInner => {
-                    itemInner.classList.remove('calculator__main__list__item-active');
+        this.#itemsElementsArray.forEach((itemElement, index) => {
+            itemElement.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.#itemsElementsArray.forEach(item => {
+                    item.classList.remove('calculator__main__list__item-active');
                 });
-                item.classList.add('calculator__main__list__item-active');
+                itemElement.classList.add('calculator__main__list__item-active');
+                this.#value = itemElement.querySelector('p').innerHTML;
+                this.#valueId = index;
             });
         });
-        
-        return list;
     }
+
+    getTitle() { return this.#title; }
+    getList() { return this.#listElement; }
+    getItemsArray() { return this.#itemsElementsArray; }
+    getValue() { return this.#value; }
+    getValueId() { return this.#valueId }
 }
 
 class CalculatorModal extends Modal {
-    #title = createElementByHTML('h2', 'modal__title calculator__main__title', 'Выберите услугу, которая Вас интересует');
-    #mainList; #listsArray = [];
-    #listsBlock; #listsBlockWrap;
-    #currentListIndex; #btnsWrap;
+    #mainBlock; #mainWrap; #btnsWrap; #sidebar;
+    #mainList; #mainListInstance; #mainListElement;  
+    #currentListIndex = 0; #currentList;
+    #selectedListInstancesArray = []; #listsCount; #listsArray = []; #selectedListsArray = []; #selectionData = [];
+    #discountElement; #discountAmount = 0; #maxDiscountAmount = 35; #discountAmountStep; #discountAmountElement;
+    #titleElement;
+    #form; #mainOffer;
 
-    constructor(calcList) {
+    constructor(calcArray) { // calcArray: { mainList: {}, listsArray: {} };
         super();
+        this.#mainList = calcArray.mainList;
+        this.#listsArray = calcArray.listsArray;
+        this.#createModal();
+        this.window.classList.add('calculator');
         this.modal.classList.add('calculator-modal');
-        this.#mainList = new CalculatorList(this.#title, calcList.mainList.items);
-        calcList.listsArray.forEach(listsList => {
-            let lists = [];
-            listsList.forEach(list => {
-                lists.push(new CalculatorList(list.title, list.items));
-            });
-            this.#listsArray.push(lists);
-        });
-        this.#createListsBlock();
+    }
+
+    #createModal() {
+        super.createModal();
+        this.#mainBlock = createElementByHTML('div', 'calculator__main', '');
+        this.#mainWrap = createElementByHTML('div', 'calculator__main-wrap', '');
+        this.#titleElement = createElementByHTML('h2', 'calculator__main__offer__title', 'Выберите услугу, которая Вас интересует');
         this.#createBtnsWrap();
-        this.#listsBlock.insertAdjacentElement('beforeend', this.#btnsWrap);
-        this.modal.insertAdjacentElement('beforeend', this.#listsBlock);
-        this.window.insertAdjacentElement('beforeend', this.modal);
-    }
-
-    #createListsBlock() {
-        this.#listsBlock = createElementByHTML('div', 'calculator__main', '');
-        this.#listsBlockWrap = createElementByHTML('div', 'calculator__main-wrap', '');
-        this.#listsBlock.insertAdjacentElement('beforeend', this.#title);
-        this.#listsBlockWrap.insertAdjacentElement('beforeend', this.#mainList.getList());
-        this.#listsBlock.insertAdjacentElement('beforeend', this.#listsBlockWrap);
-        this.#currentListIndex = 0;
-    }
-
-    #selectList() {
-        
-    }
-
-    #switchList = (step) => {
-        this.#listsArray[this.#currentListIndex].remove();
-        this.#currentListIndex += step;
-        this.#listsBlockWrap.insertAdjacentElement('beforeend', this.#listsArray[this.#currentListIndex]);
+        this.#createSidebar();
+        this.#mainListInstance = new CalculatorList(this.#mainList.title, this.#mainList.items);
+        this.#mainListElement = this.#mainListInstance.getList();
+        this.#mainWrap.insertAdjacentElement('beforeend', this.#titleElement);
+        this.#mainWrap.insertAdjacentElement('beforeend', this.#mainListElement);
+        this.#mainBlock.insertAdjacentElement('beforeend', this.#mainWrap);
+        this.#mainBlock.insertAdjacentElement('beforeend', this.#btnsWrap);
+        this.modal.insertAdjacentElement('beforeend', this.#mainBlock);
+        this.modal.insertAdjacentElement('beforeend', this.#sidebar);
+        this.#currentList = this.#mainListInstance;
     }
 
     #createBtnsWrap() {
         this.#btnsWrap = createElementByHTML('div', 'calculator__main__btns-block', '');
+
         let btnPrev = createElementByHTML('div', 'calculator__main__btn calculator__main__btn-prev btn-second1', 'Назад');
         let btnNext = createElementByHTML('div', 'calculator__main__btn calculator__main__btn-next btn1', 'Далее');
 
-        btnPrev.addEventListener('click', () => {
+        btnPrev.addEventListener('click', (e) => {
+            e.preventDefault();
             this.#switchList(-1);
         });
-        btnNext.addEventListener('click', () => {
+
+        btnNext.addEventListener('click', (e) => {
+            e.preventDefault();
             this.#switchList(1);
         });
 
         this.#btnsWrap.insertAdjacentElement('beforeend', btnPrev);
         this.#btnsWrap.insertAdjacentElement('beforeend', btnNext);
     }
+
+    #createSidebar() {
+        this.#sidebar = createElementByHTML('div', 'calculator__sidebar', '');
+        this.#createDiscountElement();
+        this.#sidebar.insertAdjacentElement('beforeend', this.#discountElement);
+    }
+
+    #createDiscountElement() {
+        this.#discountElement = createElementByHTML('div', 'calculator__sidebar__discount', '<span>Ваша скидка: </span>');
+        this.#discountAmountElement = createElementByHTML('span', '', `${this.#discountAmount} %`);
+        let iconUp = createElementByHTML('div', 'calculator__sidebar__discount-up', '<div><hr><hr></div><div><hr><hr></div>');
+        this.#discountElement.insertAdjacentElement('beforeend', this.#discountAmountElement);
+        this.#discountElement.insertAdjacentElement('beforeend', iconUp);
+    }
+
+    #changeDiscount(value = null) {
+        if (value === null)
+            this.#discountAmount = this.#currentListIndex * this.#discountAmountStep;
+        else 
+            this.#discountAmount = value;
+
+        this.#discountAmountElement.textContent = Math.round(this.#discountAmount) + " %";
+    }
+
+    #switchList(step) { // step == 1 / -1
+        if (step === 1 && this.#currentList.getValueId() === null) 
+            return;
+
+        this.#currentListIndex += step;
+
+        if (this.#currentListIndex < 0) {
+            this.#currentListIndex = 0;
+            return;
+        } else if (this.#currentListIndex > this.#listsCount) {
+            this.#openCallForm();
+            this.#changeDiscount(this.#maxDiscountAmount);
+            return;
+        }
+
+        this.#mainWrap.style.transform = "scaleY(0)";
+        setTimeout(() => {
+    
+            if (this.#currentListIndex === 1 && step === 1) {
+                this.#selectedListInstancesArray = this.#createListElementsArray(this.#listsArray[this.#currentList.getValueId()]);
+                this.#listsCount = this.#selectedListInstancesArray.length;
+                this.#discountAmountStep = this.#maxDiscountAmount / (this.#listsCount + 1);
+            }
+    
+            if (this.#currentListIndex === 0) {
+                this.#currentList = this.#mainListInstance;
+                this.#selectedListInstancesArray[0].getList().remove();
+            }
+            else {
+                this.#currentList = this.#selectedListInstancesArray[this.#currentListIndex-1];
+                if (this.#currentListIndex === 1 && step === 1)
+                    this.#mainListElement.remove();
+                else 
+                    this.#selectedListInstancesArray[this.#currentListIndex-1-step].getList().remove();
+            }
+    
+            this.#mainWrap.insertAdjacentElement('beforeend', this.#currentList.getList());
+            this.#titleElement.textContent = this.#currentList.getTitle();
+            this.#changeDiscount();
+            this.#mainWrap.style.transform = "scaleY(1)";
+        }, 400);
+    }
+
+    #createListElementsArray(listsArray) {
+        let listElementsArray = [];
+
+        listsArray.forEach(list => {
+            listElementsArray.push(new CalculatorList(list.title, list.items));
+        });
+
+        return listElementsArray;
+    }
+
+    #openCallForm() {
+        this.#mainWrap.remove();
+        this.#discountElement.remove();
+        this.#btnsWrap.remove();
+
+        this.#createMainOffer();
+        this.#createForm();
+
+        this.#mainBlock.insertAdjacentElement('beforeend', this.#mainOffer);
+        this.#sidebar.insertAdjacentElement('beforeend', this.#form);
+    }
+
+    #createMainOffer() {
+        this.#mainOffer = createElementByHTML('div', 'calculator__main__offer', '');
+        this.#titleElement.textContent = "Пожалуйста, оставьте свой номер телефона, чтобы мы смогли с Вами связаться. А также ответить на другие вопросы, касаемые лечения.";
+        
+        let desc1 = createElementByHTML('p', 'calculator__main__offer__desc', 'А также для всех жителей города действует акция на первое посещение стоматологии:');
+        let desc2 = createElementByHTML('p', 'calculator__main__offer__desc', '*Акция действует для лиц старше 30 лет');
+        let descList = createElementByHTML('ul', 'calculator__main__offer__desc-list dashed', `<li>Бесплатные приемы</li><li>3D-снимок или снимок ОПТГ</li><li>План лечения и все это совершенно <b>бесплатно</b></li>`);
+        let discountBlock = createElementByHTML('div', 'calculator__main__offer__discount', `<span>Ваша скидка: </span>
+        <span class="calculator__main__offer__discount-num">35 %</span>`);
+        
+        this.#mainOffer.insertAdjacentElement('beforeend', this.#titleElement);
+        this.#mainOffer.insertAdjacentElement('beforeend', desc1);
+        this.#mainOffer.insertAdjacentElement('beforeend', descList);
+        this.#mainOffer.insertAdjacentElement('beforeend', desc2);
+        this.#mainOffer.insertAdjacentElement('beforeend', discountBlock);
+    }
+
+    #createForm() {
+        this.#form = createElementByHTML('form', 'calculator__sidebar__form', '');
+
+        let nameInputBlock = createElementByHTML('div', 'calculator__sidebar__form__input-block', `<span>Введите имя</span><input required type="text" name="name" placeholder="Имя">`);
+        let phoneInputBlock = createElementByHTML('div', 'calculator__sidebar__form__input-block', `<span>Введите телефон</span><input required type="tel" name="phone" placeholder="Введите телефон">`);
+        let button = createElementByHTML('button', 'calculator__sidebar__form__btn btn1', 'Записаться на прием');
+        button.type = 'submit';
+        let policityBlock = createElementByHTML('div', 'calculator__sidebar__form__check-block check-block', `<input type="checkbox" checked name="agreement"><span>C <a href="#">Положение об обработке персональных данных</a> и <a href="#">Политикой конфиденциальности</a> ознакомлен и согласен</span>`);
+
+        this.#form.insertAdjacentElement('beforeend', nameInputBlock);
+        this.#form.insertAdjacentElement('beforeend', phoneInputBlock);
+        this.#form.insertAdjacentElement('beforeend', button);
+        this.#form.insertAdjacentElement('beforeend', policityBlock);
+
+        this.#recordSelectionData();
+        this.#selectionData.forEach((item, index) => {
+            let input = document.createElement('input');
+            input.type = 'hidden'; input.value = item; 
+            input.name = `question_${index+1}`;
+            this.#form.insertAdjacentElement('beforeend', input);
+        });
+    }
+
+    #recordSelectionData() {
+        this.#selectionData.push(this.#mainListInstance.getTitle() + ": " + this.#mainListInstance.getValue());
+        this.#selectedListInstancesArray.forEach(list => {
+            this.#selectionData.push(list.getTitle() + ": " + list.getValue());
+        });
+    }
 }
 
 var calculator = new CalculatorModal(calcArray);
 document.querySelectorAll('[data-calc]').forEach(calc => {
     calc.addEventListener('click', () => calculator.draw(document.querySelector('body'), 'beforeend'));
-    phoneMask();
 });
